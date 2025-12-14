ce86b1183d6895a0f85dd21c2802a355c4633b39
remed <129jinxed@gmail.com>
2025-12-14 20:31:35 +0200

    Add SaleReporter

---
+++ src/sales/SalesReporter.java
@@ -0,0 +1,39 @@
+package sales;
+
+import java.util.Map;
+import enums.*;
+
+public class SalesReporter {
+    private int totalSalesCount;
+    private double totalRevenue;
+    private Map<Category, Double> revenuePerCategory;
+
+
+    public SalesReporter(int totalSalesCount, double totalRevenue, Map<Category, Double> revenuePerCategory) {
+        this.totalSalesCount = totalSalesCount;
+        this.totalRevenue = totalRevenue;
+        this.revenuePerCategory = revenuePerCategory;
+    }
+
+    public Category findBestSellingCategory() {
+        Category bestCategory = null;
+        double maxRevenue = -1;
+
+        for (Map.Entry<Category, Double> entry : revenuePerCategory.entrySet()) {
+            if (entry.getValue() > maxRevenue) {
+                maxRevenue = entry.getValue();
+                bestCategory = entry.getKey();
+            }
+        }
+
+        return bestCategory;
+    }
+
+    public int getTotalSalesCount() { return totalSalesCount; }
+    public double getTotalRevenue() { return totalRevenue; }
+    public Map<Category, Double> getRevenuePerCategory() { return revenuePerCategory; }
+    public double getAverageSalesValue() {
+        if (totalSalesCount == 0) return 0;
+        return totalRevenue / totalSalesCount;
+    }
+}
